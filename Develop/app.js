823748c13fc9a9515e8127d0be574d240c202eaa
Mind <Mind.Narusevicius@proton.me>
2023-01-19 16:53:51 +0000

    Completed app.js file

--- Develop/app.js
+++ Develop/app.js
@@ -1,9 +1,9 @@
 $(() => {
-  let today = moment().format("dddd, MMMM Do");
-  let currentHour = moment().format("H A");
+  let $today = moment().format("dddd, MMMM Do");
+  let $currentHour = moment().format("H A");
 
   // Set Today
-  $("#currentDay").text(today);
+  $("#currentDay").text($today);
 
   let $workingHours = [
     { time: "9 AM", event: "" },
@@ -17,16 +17,46 @@ $(() => {
     { time: "5 PM", event: "" },
   ];
 
+  // load data from localstorage
+  let $data = JSON.parse(localStorage.getItem("planer_data"));
+
+  if ($data !== null) {
+    $workingHours = $data;
+  }
+
+  function getTimeColor(time) {
+    let $currentTime = moment($currentHour, "H A");
+    let $givenTime = moment(time, "H A");
+
+    if ($currentTime.isBefore($givenTime)) {
+      return "future";
+    } else if ($currentTime.isAfter($givenTime)) {
+      return "past";
+    } else {
+      return "present";
+    }
+  }
+
   $.each($workingHours, (index, value) => {
     let $div = $("<div>", { class: "time-block", id: `${index}` });
     let $content = `
       <div class="row no-gutters input-group">
         <div class="col-md-1 hour pt-3">${value.time}</div>
-        <textarea class="form-control description present">${value.event}</textarea>
-        <button class="btn col-sm-2 col-lg-1 saveBtn"><i class="far fa-save"></i></button>
+        <textarea class="form-control description ${getTimeColor(
+          value.time
+        )}">${value.event}</textarea>
+        <button class="btn col-sm-2 col-lg-1 save-btn"><i class="far fa-save"></i></button>
       </div>
     `;
     $div.html($content);
     $(".container").append($div);
+  });
+
+  // Handle save button click
+  $(".save-btn").on("click", function (e) {
+    let $blockIndex = Number($(e.target).closest(".time-block").attr("id"));
+    let $eventText = $(e.target).parent().siblings("textarea").val();
+    $workingHours[$blockIndex].event = $eventText;
+    localStorage.setItem("planer_data", JSON.stringify($workingHours));
   });
 });
